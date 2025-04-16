7d83ab0a9381146af2d6536941de110d129b94d4
shahahb 13900824 <shahabeddin.miri@gmail.com>
2025-04-16 23:00:34 +0330

    Update prom.py

--- prom.py
+++ prom.py
@@ -1,19 +1,56 @@
 import os
-
-os.system("pip install speech_recognition")
-os.system("pip install pyttsx3")
-os.system("pip install  os")
-os.system("pip install psutil")
-os.system("pip install subprocess")
-os.system("pip install pygetwindow")
-os.system("pip install pyautogui")
-os.system("pip install webbrowser")
-os.system("pip install googletrans")
-os.system("pip install requests")
-os.system("pip install shutil")
-os.system("pip install pycaw")
-os.system("pip install pyautogui")
-os.system("pip install selenium")
+try:
+    import speech_recognition
+except:
+    os.system("pip install speech_recognition")
+try:
+    import pyttsx3
+except:
+    os.system("pip install pyttsx3")
+try:
+    import os
+except:
+    os.system("pip install  os")
+try:
+    import psutil
+except:
+    os.system("pip install psutil")
+try:
+    import subprocess
+except:
+    os.system("pip install subprocess")
+try:
+    import pygetwindow
+except:
+    os.system("pip install pygetwindow")
+try:
+    import pyautogui
+except:
+    os.system("pip install pyautogui")
+try:
+    import webbrowser
+except:
+    os.system("pip install webbrowser")
+try:
+    import googletrans
+except:
+    os.system("pip install googletrans")
+try:
+    import requests
+except:
+    os.system("pip install requests")
+try:
+    import shutil
+except:
+    os.system("pip install shutil")
+try:
+    import pycaw
+except:
+    os.system("pip install pycaw")
+try:
+    import selenium
+except:
+    os.system("pip install selenium")
 
 
 
